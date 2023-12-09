fc093e10185fc71251f2b67b0677208cd0c8c342
alexanderyar <103504393+alexanderyar@users.noreply.github.com>
2023-12-08 17:55:16 -0700

    init

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  key: Key;
+  abstract door: boolean;
+  protected tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+  door = false;
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) this.door = true;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +51,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
